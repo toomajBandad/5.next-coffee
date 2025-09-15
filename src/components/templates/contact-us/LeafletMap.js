38af8cce9b0c2b0fab4de7393153abf5bf2c4a54
toomajbandad <tbandad@gmail.com>
2025-09-15 17:36:49 +0200

    final update for leaflet

--- src/components/templates/contact-us/LeafletMap.js
+++ src/components/templates/contact-us/LeafletMap.js
@@ -1,15 +1,15 @@
-'use client';
-import React, { useEffect, useState } from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import markerIconPng from 'leaflet/dist/images/marker-icon.png';
-import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+"use client";
+import React, { useEffect, useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import markerIconPng from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
 export default function LeafletMap({ center, position, children }) {
   const [icon, setIcon] = useState(null);
 
   useEffect(() => {
-    const L = require('leaflet');
+    const L = require("leaflet");
     const customIcon = new L.Icon({
       iconUrl: markerIconPng,
       shadowUrl: markerShadow,
@@ -25,7 +25,10 @@ export default function LeafletMap({ center, position, children }) {
   if (!icon) {
     return (
       <>
-        <div className="w-full h-[400px] rounded-lg overflow-hidden shadow-md" aria-label="Map loading" />
+        <div
+          className="w-full h-[400px] rounded-lg overflow-hidden shadow-md"
+          aria-label="Map loading"
+        />
         <div className="mt-4">{children}</div>
       </>
     );
@@ -34,17 +37,25 @@ export default function LeafletMap({ center, position, children }) {
   return (
     <div>
       <div className="w-full h-[400px] rounded-lg overflow-hidden shadow-md">
-        <MapContainer center={center} zoom={13} scrollWheelZoom={false} className="h-full w-full">
+        <MapContainer
+          center={center}
+          zoom={16}
+          scrollWheelZoom={false}
+          className="h-full w-full"
+        >
           <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution="&copy; CartoDB"
+            url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
           />
+
           <Marker position={position} icon={icon}>
             <Popup>Next Coffee</Popup>
           </Marker>
         </MapContainer>
       </div>
-      <div className="mt-4">{children}</div>
+      <div className="bg-white px-4 py-3 rounded-none shadow text-center text-sm text-gray-700">
+        {children}
+      </div>
     </div>
   );
 }
